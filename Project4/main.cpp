7088e4ea274652fb3149475b01150ff6e5dfbafd
mariekf <m.k.foss@astro.uio.no>
2015-10-23 11:29:21 +0200

    added simple code

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -6,26 +7,32 @@ int main()
 {
     double k = 1; // Boltzmann's constant
     int L,N;
-    double T; // units kT/J
-    double J,spin,beta;
-    double E,M,Cv,chi,Z;
+    double T=1; // units kT/J
+    double beta = 1/(k*T);
+    double Cv,chi,Z;
     double E_exp,M_exp,E_exp2,M_exp2,sigmaE,sigmaM; // Expectation values
     Z = 0;
     E_exp = 0;
     M_exp = 0;
     E_exp2 = 0;
     M_exp2 = 0;
-    beta = 1/(k*T);
 
     // Periodic boundary conditions and the Metropolis algorithm
 
     // a
     L = 2;
     N = 2*2;
+    double *spin = new double[L];
+    double *E = new double[N];
+    double *M = new double[N];
     for(int i=0;i<N;i++){
-        for(int k=i-1;k<=i+1;k++){
-            for(int j=i-1;j<=i+1;j++)
-                E[i] -= J*spin[k]*spin[l];
+        E[i] = 0;
+        M[i] = 0;
+        for(int l=0;l<L;l++)
+            spin[l] = 1;
+        for(int k=i;k<L;k++){
+            E[i] += spin[k]*spin[k+1];
+            M[i] += spin[k];
         }
         Z += exp(-beta*E[i]);
         E_exp +=  E[i]*exp(-beta*E[i]);
@@ -45,6 +52,10 @@ int main()
     cout << "<M> = " << M_exp << endl;
     cout << "Cv = " << Cv << endl;
     cout << "chi = " << chi << endl;
+
+    delete[] spin;
+    delete[] E;
+    delete[] M;
 
     // b, Ising model; <E>, <|M|>, Cv, chi as functions of T
     // c, <E>, <M> as functions of MC cycles
