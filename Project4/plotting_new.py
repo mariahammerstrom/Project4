c2f62cc9c92466bb68ef5b17a5585b73f2602954
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-11-06 12:39:15 +0100

    Plot program.

--- Project4/plotting_new.py
+++ Project4/plotting_new.py
@@ -38,8 +38,8 @@ def read_file(filename):
     return T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC
 
 
-
-def exp_values_MC(L,temps):
+    
+def exp_values_MC(L,temps,random):
     # Input: L = array or list with dimension of lattice(s), temps = array or list of temperatures
     # Output: Plots of expectation values vs. # of Monte Carlo cycles
     
@@ -71,33 +71,34 @@ def exp_values_MC(L,temps):
     
     for i in range(len(temps)):
         # Read file
-        filenameMC = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_MC_%d_%.6f.txt' % (L,temps[i])
+        filenameMC = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_MC_%d_%.1f_%d.txt' % (L,temps[i],random)
+        #filenameMC = 'ExpectationValues_MC_%d_%.1f_%d.txt' % (L,temps[i],random)
         T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC = read_file(filenameMC)
         
         # Fill plots with values
         plt.figure(1)
         plt.plot(MC_cycles,E_avg,label=r'$T =$ %.2f' % temps[i])
-        plt.ylim(-2.2,-1.0)
+        #plt.ylim(-2.2,-1.0)
         plt.legend()
         
         plt.figure(2)
         plt.plot(MC_cycles,M_absavg,label=r'$T =$ %.2f' % temps[i])
-        plt.ylim(0.7,1.1)
+        #plt.ylim(0.7,1.1)
         plt.legend()
         
         plt.figure(3)
         plt.plot(MC_cycles,C_v,label=r'$T =$ %.2f' % temps[i])
-        plt.ylim(-0.5,1.0)
+        #plt.ylim(-0.5,1.0)
         plt.legend()
         
         plt.figure(4)
         plt.plot(MC_cycles,X,label=r'$T =$ %.2f' % temps[i])
-        plt.ylim(-0.1,0.3)
+        #plt.ylim(-0.1,0.3)
         plt.legend()
         
         plt.figure(5)
         plt.plot(MC_cycles,AC,label=r'$T =$ %.2f' % temps[i])
-        plt.ylim(-0.5,2.0)
+        #plt.ylim(-0.5,2.0)
         plt.legend()
     
     plt.show()
@@ -105,8 +106,8 @@ def exp_values_MC(L,temps):
     return
 
 
-def exp_values_T(L):
-    # Input: L = array or list of lattice sizes
+def exp_values_T(L_list,random):
+    # Input: L_list = array or list of lattice sizes
     # Output: Plots expectation values vs. temperature
     
     # Define plots
@@ -135,34 +136,34 @@ def exp_values_T(L):
     plt.xlabel(r'$T \mathrm{[kT/J]}$')
     plt.ylabel('# of accepted configurations',size=12)
 
-    for i in range(len(L)):
+    for i in range(len(L_list)):
         # Read file
-        filenameT = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_temp_%d.txt' % (L[i])
+        filenameT = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_temp_%d_%d.txt' % (L_list[i],random)
         T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC = read_file(filenameT)
         
         # Fill plots with values
         plt.figure(6)
-        plt.plot(T,E_avg,label='L = %d' % L[i])
-        plt.ylim(-2.5,0)
+        plt.plot(T,E_avg,label='L = %d' % L_list[i])
+        #plt.ylim(-2.5,0)
         plt.legend()
         
         plt.figure(7)
-        plt.plot(T,M_absavg,label='L = %d' % L[i])
-        plt.ylim(0,1.4)
+        plt.plot(T,M_absavg,label='L = %d' % L_list[i])
+        #plt.ylim(0,1.4)
         plt.legend()
         
         plt.figure(8)
-        plt.plot(T,C_v,label='L = %d' % L[i])
-        plt.ylim(0,4)
+        plt.plot(T,C_v,label='L = %d' % L_list[i])
+        #plt.ylim(0,4)
         plt.legend()
         
         plt.figure(9)
-        plt.plot(T,X,label='L = %d' % L[i])
-        plt.ylim(0,250)
+        plt.plot(T,X,label='L = %d' % L_list[i])
+        #plt.ylim(0,250)
         plt.legend()
         
         plt.figure(10)
-        plt.plot(T,AC,label='L = %d' % L[i])
+        plt.plot(T,AC,label='L = %d' % L_list[i])
         plt.legend()
     
     plt.show()
@@ -170,12 +171,12 @@ def exp_values_T(L):
     return
     
     
-def probability(L,temp):
+def probability(L,temp,random):
     # Input: L = lattice size, temp = temperature
     # Output: Probability P(E) of a given E and variance of E
     
-    filenameMC = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_MC_%d_%.6f.txt' % (L,temp)
-    filenameE = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/Energy_MC_%d_%.6f.txt' % (L,temp)
+    filenameMC = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_MC_%d_%.1f_%d.txt' % (L,temp,random)
+    filenameE = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/Energy_MC_%d_%.1f_%d.txt' % (L,temp,random)
     
     T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC = read_file(filenameMC)
         
@@ -196,21 +197,52 @@ def probability(L,temp):
     print "Var(E) = %.2f" % E_var[-1]
 
     return
+
+
+def T_C_estimate(L_list,random):
+    # Input: L = array or list of lattice sizes
+    # Output: T_C = numerical estimate of critical temperature
+    
+    T_C = np.zeros(len(L_list))
+    
+    for i in range(len(L_list)):
+        filenameT = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_temp_%d_%d.txt' % (L_list[i],random)
+        T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC = read_file(filenameT)
+        # X ...
+        # T_C[i] = ...
+    
+    print "Critical temperature:"
+    print "Exact = \t 2.269"
+    
+    nu = 1
+    a = 1
+    
+    for i in range(len(L_list)):
+        T_C_estimate = T_C[i] - a*L_list[i]**(-1./nu)
+        print "Numerical = \t %.3f \t L = %d" % (T_C_estimate,L_list[i])
+    
+    return
     
 
 def main(argv):
     
-    # Expectation values vs. # of Monte Carlo cycles
+    # Change according to run!
+    random = 0 # true = 1, false = 0
     L = 2
-    temps = [1.0, 2.4]
-    exp_values_MC(L,temps)
+    temps = [1.0,2.4]
+    
+    # Expectation values vs. # of Monte Carlo cycles
+    exp_values_MC(L,temps,random)
     
     # Expectation values vs. temperature
-    L_list = [2,20,40,80]
-    #exp_values_T(L_list)
+    L_list = [2,20]
+    #exp_values_T(L_list,random)
     
     # Calculate probabilty for <E>
-    #probability(L,temps[0])
+    #probability(L,temps[0],random)
+    
+    # Calculate estimate of T_crit
+    #T_C_estimate(L_list,random)
 
 if __name__ == "__main__":
     main(sys.argv[1:]) 
