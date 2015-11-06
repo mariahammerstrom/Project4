ed0e0f652572084bf2e71086781c34ad64a66feb
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-11-06 07:01:25 +0100

    Latest version!

--- Project4/plotting_new.py
+++ Project4/plotting_new.py
@@ -1,10 +1,14 @@
 """THE 2-DIMENSIONAL ISING MODEL
 
 A program that plots:
-1) the total number of accepted configurations as a function of the 
-total number of Monte Carlo cycles,
-2) the total number of accepted configurations as a function of temperature
-for a 2-dimensional Ising model using the Metropolis algorithm."""
+    1) Expectation values,
+    2) Number of accepted configurations, and
+    3) Number of Monte Carlo cycles
+as a function of temperature and number of Monte Carlo cycles.
+
+The program also calculates the probability of the average energy
+and compares it to the standard deviation for the average energy."""
+
 
 import sys
 import numpy as np
@@ -14,6 +18,9 @@ rc('font',**{'family':'serif'})
 
 
 def read_file(filename):
+    # Input: filename
+    # Output: Arrays of data values
+    
     data = np.loadtxt(filename,unpack=True) # Read data
     
     # Model   
@@ -29,41 +36,12 @@ def read_file(filename):
     AC = data[7]		    # Accepted configurations
     
     return T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC
-    
 
-def probability(filenameMC,filename_E,L):
-    data_MC = np.loadtxt(filenameMC,unpack=True)   # Read data
-    T = data_MC[0]                                 # Temperature [kT/J]
-    MC_cycles = data_MC[1]                         # Number of Monte Carlo cycles
-    E_avg = data_MC[2]                             # Expectation value of energy
-    E_var = data_MC[6]                             # Variance of energy
-        
-    E = np.loadtxt(filename_E,unpack=True)         # Energies
-    
-    # Round-offs to make counting possible
-    precision = 2
-    E_avg = np.around(E_avg,decimals=precision)
-    E = np.around(E,decimals=precision)
-
-    # Calculate probability
-    no_elements = len(E)
-    counter = list(E).count(E_avg[-1])
-    prob = float(counter)/no_elements
-    
-    print "T = %.2f" % T[0] 
-    print "MC cycles = %d" % MC_cycles[-1]
-    print
-    
-    print "Probability:"
-    print "P(E) = %.2f" % prob
-    print "Var(E) = %.2f" % E_var[-1]
-
-    return
 
 
 def exp_values_MC(L,temps):
-    # L = array or list with dimension of lattice(s)
-    # temps = array or list of temperatures
+    # Input: L = array or list with dimension of lattice(s), temps = array or list of temperatures
+    # Output: Plots of expectation values vs. # of Monte Carlo cycles
     
     # Define plots
     plt.figure(1)
@@ -92,87 +70,95 @@ def exp_values_MC(L,temps):
     plt.ylabel('Accepted configurations',size=12)    
     
     for i in range(len(temps)):
-        
         # Read file
-        filenameMC = 'ExpectationValues_MC_%d_%.6f.txt' % (L,temps[i])
+        filenameMC = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_MC_%d_%.6f.txt' % (L,temps[i])
         T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC = read_file(filenameMC)
         
-        # Plot expectation values
+        # Fill plots with values
         plt.figure(1)
         plt.plot(MC_cycles,E_avg,label=r'$T =$ %.2f' % temps[i])
+        plt.ylim(-2.2,-1.0)
         plt.legend()
         
         plt.figure(2)
         plt.plot(MC_cycles,M_absavg,label=r'$T =$ %.2f' % temps[i])
+        plt.ylim(0.7,1.1)
         plt.legend()
         
         plt.figure(3)
         plt.plot(MC_cycles,C_v,label=r'$T =$ %.2f' % temps[i])
+        plt.ylim(-0.5,1.0)
         plt.legend()
         
         plt.figure(4)
         plt.plot(MC_cycles,X,label=r'$T =$ %.2f' % temps[i])
+        plt.ylim(-0.1,0.3)
         plt.legend()
         
         plt.figure(5)
         plt.plot(MC_cycles,AC,label=r'$T =$ %.2f' % temps[i])
+        plt.ylim(-0.5,2.0)
         plt.legend()
     
     plt.show()
     
     return
 
-def exp_values_T(L,T):
-    # L = lattice size
-    # T = temperature
+
+def exp_values_T(L):
+    # Input: L = array or list of lattice sizes
+    # Output: Plots expectation values vs. temperature
     
     # Define plots
     plt.figure(6)
-    plt.title('Energy expectation value, T = %.2f' % T,size=12)
+    plt.title('Energy',size=12)
     plt.xlabel(r'$T \mathrm{[kT/J]}$',size=12)
     plt.ylabel(r'$\langle E \rangle$',size=12)
     
     plt.figure(7)
-    plt.title('Magnetism expectation value, T = %.2f' % T,size=12)
+    plt.title('Magnetism',size=12)
     plt.xlabel(r'$T \mathrm{[kT/J]}$',size=12)
     plt.ylabel(r'$\langle M \rangle$',size=12)
     
     plt.figure(8)
-    plt.title('Heat capacity expectation value, T = %.2f' % T,size=12)
+    plt.title('Heat capacity',size=12)
     plt.xlabel(r'$T \mathrm{[kT/J]}$',size=12)
     plt.ylabel(r'$C_V$',size=12)
     
     plt.figure(9)
-    plt.title('Susceptibility expectation value, T = %.2f' % T,size=12)
+    plt.title('Susceptibility',size=12)
     plt.xlabel(r'$T \mathrm{[kT/J]}$',size=12)
     plt.ylabel(r'$\chi$',size=12)
     
     plt.figure(10)
-    plt.title('Accepted configurations vs. temperature, T = %.2f' % T,size=12)
+    plt.title('Accepted configurations vs. temperature',size=12)
     plt.xlabel(r'$T \mathrm{[kT/J]}$')
     plt.ylabel('# of accepted configurations',size=12)
 
     for i in range(len(L)):
-        
         # Read file
-        filenameT = 'ExpectationValues_temp_%d_%.6f.txt' % (L[i],T)
+        filenameT = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_temp_%d.txt' % (L[i])
         T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC = read_file(filenameT)
         
-        # Plot
+        # Fill plots with values
         plt.figure(6)
         plt.plot(T,E_avg,label='L = %d' % L[i])
+        plt.ylim(-2.5,0)
         plt.legend()
         
         plt.figure(7)
         plt.plot(T,M_absavg,label='L = %d' % L[i])
+        plt.ylim(0,1.4)
         plt.legend()
         
         plt.figure(8)
         plt.plot(T,C_v,label='L = %d' % L[i])
+        plt.ylim(0,4)
         plt.legend()
         
         plt.figure(9)
         plt.plot(T,X,label='L = %d' % L[i])
+        plt.ylim(0,250)
         plt.legend()
         
         plt.figure(10)
@@ -182,19 +168,49 @@ def exp_values_T(L,T):
     plt.show()
     
     return
+    
+    
+def probability(L,temp):
+    # Input: L = lattice size, temp = temperature
+    # Output: Probability P(E) of a given E and variance of E
+    
+    filenameMC = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/ExpectationValues_MC_%d_%.6f.txt' % (L,temp)
+    filenameE = '../build-Project4-Desktop_Qt_5_5_0_clang_64bit-Debug/Energy_MC_%d_%.6f.txt' % (L,temp)
+    
+    T,MC_cycles,E_avg,M_absavg,C_v,X,E_var,AC = read_file(filenameMC)
+        
+    E = np.loadtxt(filenameE,unpack=True)         # Energies
+    
+    # Round-offs to make counting possible
+    precision = 2 # Number of decimal points
+    E_avg = np.around(E_avg,decimals=precision)
+    E = np.around(E,decimals=precision)
+
+    # Calculate probability
+    no_elements = len(E)
+    counter = list(E).count(E_avg[-1])
+    prob = float(counter)/no_elements
+    
+    print "Probability: (T = %.2f)" % temp
+    print "P(E) = %.2f" % prob
+    print "Var(E) = %.2f" % E_var[-1]
+
+    return
+    
 
 def main(argv):
-
-    # Change depending on situation!!
-    L = 20
+    
+    # Expectation values vs. # of Monte Carlo cycles
+    L = 2
     temps = [1.0, 2.4]
+    exp_values_MC(L,temps)
     
-    #exp_values_MC(L,temps)
+    # Expectation values vs. temperature
+    L_list = [2,20,40,80]
+    #exp_values_T(L_list)
     
-    L = [2,20]
-    exp_values_T(L,temps[0])
-    
-        
+    # Calculate probabilty for <E>
+    #probability(L,temps[0])
 
 if __name__ == "__main__":
     main(sys.argv[1:]) 
