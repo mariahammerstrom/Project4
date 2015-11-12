6fe62e0e65f435326095b08fae7c03a617bb3d0d
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-11-12 14:39:41 +0100

    Changes!

--- Project4/plotting.py
+++ Project4/plotting.py
@@ -1,5 +1,4 @@
 """
-
 THE 2-DIMENSIONAL ISING MODEL
 
 A program that plots:
@@ -45,7 +44,8 @@ def read_file(filename):
 
     
 def exp_values_MC(L,temps,random):
-    # Input: L = array or list with dimension of lattice(s), temps = array or list of temperatures
+    # Input: L = array or list with dimension of lattice(s), temps = array or list of temperatures,
+    # random = 0 (non-random initial spin matrix) or 1 (random initial spin matrix)
     # Output: plots of expectation values vs. # of Monte Carlo cycles
     
     # Define plots
@@ -97,20 +97,10 @@ def exp_values_MC(L,temps,random):
         # Fill plots with values
         plt.figure(1)
         plt.plot(MC_cycles,E_avg,color,label=r'$T =$ %.2f' % temps[i])
-        #coefficients = np.polyfit(MC_cycles,E_avg,6)
-        #polynomial = np.poly1d(coefficients)
-        #E_avg_smooth = polynomial(MC_cycles)
-        #plt.plot(MC_cycles,E_avg_smooth,label=r'$T =$ %.2f' % temps[i])
-        #plt.ylim(-2.2,-1.0)
         plt.legend()
         
         plt.figure(2)
         plt.plot(MC_cycles,M_absavg,color,label=r'$T =$ %.2f' % temps[i])
-        #coefficients = np.polyfit(MC_cycles,M_absavg,6)
-        #polynomial = np.poly1d(coefficients)
-        #M_absavg_smooth = polynomial(MC_cycles)
-        #plt.plot(MC_cycles,M_absavg_smooth,label=r'$T =$ %.2f' % temps[i])
-        #plt.ylim(0.7,1.1)
         plt.legend()
         
         plt.figure(3)
@@ -198,7 +188,7 @@ def exp_values_T(L_list,random):
     return
 
 def exp_rand_MC(L,temp,random_list):
-    # Input: L = lattice size, temp = temperature, random = 0 (non-random spin matrix) or 1 (random spin matrix)
+    # Input: L = lattice size, temp = temperature, random = 0 (non-random initial spin matrix) or 1 (random initial spin matrix)
     # Ouput: plot of expectation values <E> and <M> for random and non-random spin matrix
     
     # Define plots
@@ -243,9 +233,11 @@ def exp_rand_MC(L,temp,random_list):
     return
     
     
-def probability(L,temp,random):
-    # Input: L = lattice size, temp = temperature
-    # Output: probability P(E) of a given E and variance of E
+def probability(L,temp,random,cutoff):
+    # Input: L = lattice size, temp = temperature, 
+    # random = 0 (non-random initial spin matrix) or 1 (random initial spin matrix),
+    # cutoff = no. of MC cycles to ignore.
+    # Output: probability P(E) of a given E and variance of E.
     
     filenameMC = '../Datasets/ExpectationValues_MC_%d_%.1f_%d.txt' % (L,temp,random)
     filenameE = '../Datasets/Energy_MC_%d_%.1f_%d.txt' % (L,temp,random)
@@ -258,7 +250,7 @@ def probability(L,temp,random):
     precision = 2 # Number of decimal points
     E_avg = np.around(E_avg,decimals=precision)
     E = np.around(E,decimals=precision)
-    E = E[3000:-1] # Only include energies after most likely state is reached
+    E = E[cutoff:-1] # Only include energies after most likely state is reached
 
     # Calculate probability
     no_elements = len(E)
@@ -275,18 +267,17 @@ def probability(L,temp,random):
 
 
 def T_C_estimate(L_list,random):
-    # Input: L = array or list of lattice sizes
+    # Input: L = array or list of lattice sizes, random = 0 (non-random initial spin matrix) or 1 (random initial spin matrix)
     # Output: T_C = numerical estimate of critical temperature
     
     print "CRITICAL TEMPERATURE:"
-    
     nu = 1
     a = 0.25
     T_C_exact = 2.269 # Exact critical temperature [kT/J]
+    T_max = np.zeros(len(L_list))
         
     print "Exact = \t %.3f" % T_C_exact
     print "a = \t \t %.2f \n" % a
-    T_max = np.zeros(len(L_list))
     
     for i in range(len(L_list)):
         filenameT = '../Datasets/ExpectationValues_temp_%d_%d.txt' % (L_list[i],random)
@@ -296,15 +287,18 @@ def T_C_estimate(L_list,random):
         # Find critical temperature from data set:
         T_max[i] = T[np.where(X == max(X))]
         T_estimate = T_max[i] - a*L_list[i]**(-1./nu)
-        print "L = %d: \t T_max = %.3f \t T_estimate = %.3f" % (L_list[i],T_max[i],T_estimate)
+        rel_error = (T_estimate - T_C_exact)/T_C_exact
+        print "L = %d: \t T_max = %.3f \t T_estimate = %.3f \t Rel.error = %.2e" % (L_list[i],T_max[i],T_estimate,rel_error)
             
     return
     
 
 def timeusage():
-    # Plot time usage
-    L_sizes = np.array([10,20,40,60,80,100])
-    time = np.array([57,232,923,2886,7320,31582]) # [s]
+    # Plot time usage for different lattice sizes L
+    
+    L_sizes = np.array([10,20,40,60,80,100])          # lattice sizes
+    time = np.array([57,232,923,2886,7320,31582])     # time usage [s]
+    
     plt.figure()
     plt.plot(L_sizes,time,'k',label='Data')
     plt.plot(L_sizes,L_sizes**2,'b--',label=r'$L^2$')
@@ -324,19 +318,19 @@ def main(argv): # Change these values according to run!
     
     # How large is the lattice?
     L = 20
-    L_list = [80,100]
+    L_list = [80,100,120]
     
     # What is the temperature?
     temp = 2.4
     temp_list = [1.0,2.4]
     
-    # Run calculations:
+    # Run calculations: Uncomment the one you want :-)
     #exp_values_MC(L,temp_list,random)      # Expectation values vs. # of Monte Carlo cycles
-    #exp_rand_MC(L,temp,random_list)        # Expectation values vs. # of Monte Carlo cycles: Different initial spin matrix!
+    #exp_rand_MC(L,temp,random_list)        # Expectation values vs. # of Monte Carlo cycles: Random vs. non-random
     #exp_values_T(L_list,random)            # Expectation values vs. temperature
-    #probability(L,temp,random)             # Calculate probabilty for <E>
-    T_C_estimate(L_list,random)            # Calculate estimate of T_crit
-    #timeusage()                         # Plot time usage to run code (based on dataset)
+    probability(L,temp,random,cutoff=3000)      # Calculate probabilty for <E>
+    #T_C_estimate(L_list,random)             # Calculate estimate of T_crit based on analytical value of the constant a
+    #timeusage()                            # Plot time usage to run code (based on dataset)
     
 
 if __name__ == "__main__":
